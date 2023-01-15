59e93ac9c41ff9f1512eae086f974827b1abedad
Yuriferr <yurifernandespreto@gmail.com>
2023-01-14 22:30:28 -0300

    primeiro

---
+++ web/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import './App.scss'
+
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+import Logo from '/yurei.png'
+import Dev from './assets/dev.png'
+
+function App() {
+
+  function Alert(){
+    toast("🦄Obrigado por utilizar este site!!!");
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <div className='logo'>
+          <img src={Logo}/>
+          <p>Yuriferr</p>
+        </div>
+
+        <div className='links'>
+          <a href='mailto:yurifernandespreto@gmail.com'>Email</a>
+          <a target='_blanck' href='https://www.linkedin.com/in/yuriferr'>Linkedin</a>
+          <a target='_blanck' href='https://github.com/Yuriferr'>GitHub</a>
+        </div>
+      </header>
+
+      <section>
+        <div className='container'>
+          <div className='containerInfo'>
+            <h1>Yuri Fernandes</h1>
+            <h3>Desenvolvedor Full Stack</h3>
+            <button onClick={Alert}>Click Me!</button>
+          </div>
+          <div className='containerImg'>
+            <img src={Dev}/>
+          </div>
+        </div>
+
+        <div className='info'>
+          <div/>
+            <p>🚀Desenvolvimento rápido</p>
+            <p>🧹Códigos otimizados</p>
+            <p>📓Rápido aprendizado para novas linguagens</p>
+        </div>
+      </section>
+      <ToastContainer />
+    </div>
+  )
+}
+
+export default App
